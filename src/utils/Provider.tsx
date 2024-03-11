d2d6269426208841f1bee5ab115a97b089313e39
wealthiduwe <wealthiduwe@gmail.com>
2024-03-11 16:36:48 -0400

    Add:Added config for react query integration

---
+++ src/utils/Provider.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+function Provider({ children }: React.PropsWithChildren) {
+	const [client] = React.useState(new QueryClient());
+
+	return (
+		<QueryClientProvider client={client}>
+			<ReactQueryStreamedHydration>
+				{children}
+			</ReactQueryStreamedHydration>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+}
+
+export default Provider;
