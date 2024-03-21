3894356d26cb75d1a1e7176e7ba262f09553b476
wealthiduwe <wealthiduwe@gmail.com>
2024-03-21 13:40:11 -0400

    Add:Added a models file for interfaces

---
+++ src/app/auth/model/model.ts
@@ -0,0 +1,33 @@
+export interface IUpdatePassword {
+	newPassword: string;
+	currentPassword: string;
+	confirmNewPassword: string;
+	resetToken: string;
+}
+
+export interface IResetPassword {
+	email: string;
+}
+
+export interface ILogin {
+	email: string;
+	password: string;
+}
+
+export interface IRegister {
+	name: string;
+	email: string;
+	password: string;
+	role?: 'user' | 'admin';
+}
+export interface IBusiness {
+	businessName: string;
+	registrationId: string;
+	businessEmail: string;
+	businessContact: string;
+	businessCreator: string;
+	country: string;
+	businessAddress: string;
+	logo?: string;
+	businessUsers?: Array<any>;
+}
