bde5accffdf581c216f7f72d064559a5e50b55c2
wealthiduwe <wealthiduwe@gmail.com>
2024-03-29 23:16:18 -0400

    Add: Added a class to verify token and get role

---
+++ src/middlewareFeatures.ts
@@ -0,0 +1,65 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+import { NextRequest } from 'next/server';
+
+class MiddlewareFeatures {
+	userInfo: any;
+	private token;
+	private isAdmin: boolean;
+	private isUser: boolean;
+	private isSuperAdmin: boolean;
+	private isAuthenticated: boolean;
+
+	constructor() {
+		this.token = cookies().get('token');
+		this.userInfo = null;
+		this.isAuthenticated = false;
+		this.isSuperAdmin = false;
+		this.isUser = false;
+		this.isAdmin = false;
+	}
+
+	// getRoleType() {
+	// 	console.log(this.userInfo?.role);
+	// 	this.isAdmin = /ADMIN/.test(this.userInfo?.role);
+	// 	this.isUser = /USER/.test(this.userInfo?.role);
+	// 	this.isSuperAdmin = /SUPER_ADMIN/.test(this.userInfo?.role);
+	// 	return this;
+	// }
+
+	get isUserAuthenticated() {
+		return this.isAuthenticated;
+	}
+
+	get isAdminRole() {
+		return /ADMIN/.test(this.userInfo?.role);
+	}
+	get isUserRole() {
+		return /USER/.test(this.userInfo?.role);
+	}
+	get isSuperAdminRole() {
+		return /SUPER_ADMIN/.test(this.userInfo?.role);
+	}
+
+	get userId() {
+		return this.userInfo.id;
+	}
+
+	verifyToken() {
+		if (!this.token) return;
+		try {
+			this.userInfo = jwt.verify(
+				this.token.value,
+				process.env.JWT_SECRET!
+			);
+
+			this.isAuthenticated = true;
+		} catch (e) {
+			this.userInfo = null;
+			this.isAuthenticated = false;
+		}
+		return this;
+	}
+}
+
+export default MiddlewareFeatures;
