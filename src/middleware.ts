2459c6993f2142fb6214ce4621a3160f22e0f58c
wealthiduwe <wealthiduwe@gmail.com>
2024-04-23 18:42:09 -0400

    Update: Updated middleware file to handle edge cases for client routes and server routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,38 @@
-import jwt from 'jsonwebtoken';
-
+import jwt, { JwtPayload } from 'jsonwebtoken';
 // This is an example using TypeScript for Next.js middleware
 import { cookies } from 'next/headers';
 import { NextResponse, NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
-	// Accessing the cookie named "sessionId"
+export function middleware(request: NextRequest, response: NextResponse) {
+	const url = request.nextUrl.clone();
+	const token = cookies().get('token') || null;
 
-	const token = cookies().get('token');
-	const loginPagePath = '/auth/login';
 	const currentPath = new URL(request.url).pathname;
-	// const sessionId = request.cookies.get('token');
 
-	// You can then use this sessionId for authentication/authorization checks,
-	// logging, or forwarding it as part of a request to an external service, etc.
-	// console.log(`Session ID: ${sessionId}`);
+	const unprotectedRoute = url.pathname.startsWith('/auth');
+	const basePath = '/';
+	const { valid, role } = verifyToken(token?.value!);
 
-	console.log(token);
-
-	// if (!token && currentPath !== loginPagePath) {
-	// 	const loginUrl = new URL('/auth/login', request.url);
-	// 	return NextResponse.redirect(loginUrl);
-	// }
-	if (token) {
-		// decodeToken(token.value);
+	//if there is no token (user isn't authenticated) and visit is made to to protected routes
+	if (!token && !unprotectedRoute) {
+		const loginUrl = new URL('/auth/login', request.url);
+		return NextResponse.redirect(loginUrl);
 	}
 
-	// Continue with the request
+	//If the logged in user tries to access the login page without logging out or visits the base path -- reroute to the dashboard
+	if (token && (unprotectedRoute || currentPath === basePath)) {
+		const dashboardUrl = /ADMIN/.test(role)
+			? new URL('/admin/dashboard', request.url)
+			: new URL('/dashboard', request.url);
+
+		return NextResponse.redirect(dashboardUrl);
+	}
+
+	//  if there's a token kindly verify:: users with unverified token should be sent to login
+	if (!valid && !unprotectedRoute) {
+		const loginUrl = new URL('/auth/login', request.url);
+		return NextResponse.redirect(loginUrl);
+	}
 
 	return NextResponse.next();
 }
@@ -34,30 +40,16 @@ export const config = {
 	matcher: [
 		'/',
 		'/dashboard/:path*',
-		'/signup',
-		'/auth/login',
-		'/api/users'
-		// '/^/api((?!/auth/(login|register))/?.*',
-		// '/(^/api(/(?!auth/(login|register)))?.*$',
-		// '/((?!api|auth/login|auth/register).*)',
-		// '/((?!/api/auth/login|/api/auth/register)|/api))'
-
-		// '/((?!api|_next/static|_next/image|favicon.ico).*)'
+		'/admin/dashboard/:path*',
+		'/auth/:path*'
 	]
 };
 
-// Function to decode the JWT token and extract the user role
-function decodeToken(token: string | null): any {
-	if (!token) return null;
+// Function to verify the JWT token
+function verifyToken(token: string | null): any {
+	if (!token) return { valid: false, role: null };
 
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-		// Assuming the payload has a "role" field
+	const { exp, role } = jwt.decode(token) as JwtPayload;
 
-		console.log(decoded);
-		return decoded;
-	} catch (error) {
-		console.error('Token verification failed:', error);
-		return null;
-	}
+	return { valid: Date.now() < exp! * 1000, role };
 }
