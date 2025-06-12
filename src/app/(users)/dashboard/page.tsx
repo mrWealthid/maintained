b84d8dd8bcde3e29c3272d5b1183c5ca1de93e6c
Wealth Iduwe <wealthiduwe@gmail.com>
2025-06-12 09:42:39 -0400

    refactor: remove unnecessary padding from main container in Home component

--- src/app/(users)/dashboard/page.tsx
+++ src/app/(users)/dashboard/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	function handleClick(val: number) {}
 
 	return (
-		<main className='flex min-h-screen gap-6 flex-col  p-24'>
+		<main className='flex min-h-screen gap-6 flex-col'>
 			<section className='flex w-full justify-between'>
 				<p className='border first-letter:text-blue-700 first-letter:text-xl border-gray-50 p-2 rounded-lg'>
 					Maintained
