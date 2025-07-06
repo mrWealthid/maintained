a2f860552e07b0820ff7f589bab145b9c5f22d1a
Wealth Iduwe <wealthiduwe@gmail.com>
2025-07-05 23:04:47 -0400

    fix(images): added a trusted domain to load images from server

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
 		CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
 		IMG_PRESET: process.env.UPLOAD_IMAGE_PRESET,
 		VIDEO_PRESET: process.env.UPLOAD_VIDEO_PRESET
+	},
+	images: {
+		domains: ['res.cloudinary.com']
 	}
 };
 
