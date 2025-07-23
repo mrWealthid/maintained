d031d35b218d9fc0f5939bbc01b47e4c8a9a414f
Wealth Iduwe <wealthiduwe@gmail.com>
2025-07-23 12:54:06 -0400

    fix:added jsx for created routes

--- src/app/(users)/dashboard/chat/page.tsx
+++ src/app/(users)/dashboard/chat/page.tsx
@@ -1,24 +1,24 @@
 'use client';
 
 import { useChat, useCompletion } from '@ai-sdk/react';
-//This is for testing the completion endpoint
-// export default function Page() {
-// 	const { completion, complete, isLoading, error } = useCompletion({
-// 		api: '/api/completion'
-// 	});
+// This is for testing the completion endpoint
+export default function Page() {
+	const { completion, complete, isLoading, error } = useCompletion({
+		api: '/api/completion'
+	});
 
-// 	return (
-// 		<div>
-// 			<button onClick={() => complete('Why is the sky blue?')}>
-// 				Generate
-// 			</button>
+	return (
+		<div>
+			<button onClick={() => complete('Why is the sky blue?')}>
+				Generate
+			</button>
 
-// 			{isLoading && <p>Loading...</p>}
-// 			{error && <p className='text-red-500'>Error: {error.message}</p>}
-// 			<div>{completion}</div>
-// 		</div>
-// 	);
-// }
+			{isLoading && <p>Loading...</p>}
+			{error && <p className='text-red-500'>Error: {error.message}</p>}
+			<div>{completion}</div>
+		</div>
+	);
+}
 
 //This is for testing the chat endpoint
 // export default function Page() {
