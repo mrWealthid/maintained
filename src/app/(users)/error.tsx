1debc48a25ff53ab50b591becb930be645874ac9
Wealth Iduwe <wealthiduwe@gmail.com>
2025-07-28 10:10:42 -0400

    fix:(production-error): added error fallback component

---
+++ src/app/(users)/error.tsx
@@ -0,0 +1,14 @@
+export default function Error({
+	error,
+	reset
+}: {
+	error: Error & { digest?: string };
+	reset: () => void;
+}) {
+	return (
+		<div>
+			<h2>Something went wrong! {error.message} </h2>
+			<h2>digest! {error.digest} </h2>
+		</div>
+	);
+}
